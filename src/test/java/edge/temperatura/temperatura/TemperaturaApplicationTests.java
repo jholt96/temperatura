5a0fdedca2167222bde005312a005faf5598d7cc
Josh Holt <jholt96@live.com>
2020-06-08 12:11:22 -0400

    initial commit

---
+++ src/test/java/edge/temperatura/temperatura/TemperaturaApplicationTests.java
@@ -0,0 +1,13 @@
+package edge.temperatura.temperatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperaturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
