78401099a90b02bfca398bbf2f42050b56b5d95f
Josh Holt <jholt96@live.com>
2020-06-08 12:43:13 -0400

    added websocket dependencies

--- src/main/java/edge/temperatura/temperatura/WebSocketConfig.java
+++ src/main/java/edge/temperatura/temperatura/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package ibm.edge.edgekafkademo;
+package edge.temperatura.temperatura;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
