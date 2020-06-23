f9014ea569824e0387248026d0da0fb6a952ccf7
jholt96 <jholt96@live.com>
2020-06-23 16:33:59 -0400

    security config update

--- src/main/java/edge/temperatura/temperatura/security/SecurityConfig.java
+++ src/main/java/edge/temperatura/temperatura/security/SecurityConfig.java
@@ -1,26 +1,42 @@
-//TODO JWT, persist remember me token, error handling, login, clean up. 
+/*
+Author: Josh Holt
+Temperatura Backend
+Versions: Spring Boot 2.3, Java 11.
 
+Purpose of Class: enables Web Security
+
+Uses JWT tokens to authenticate users. Creates JWT auth filter bean, authenticationManager bean, and the daoAuthentication bean. 
+then overrides the configure method to set the security features. uses the JWT filter, and allows access to websocket path and the management path.
+These two paths are secured by PreAuthorize due to login needing no preauth and other methods needing only ADMINS to access. 
+
+CSRF is enabled. 
+CORS is disabled for developmental purposes. 
+
+*/
 package edge.temperatura.temperatura.security;
-
-import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@Order(1)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -33,18 +49,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		this.passwordEncoder = passwordEncoder;
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Autowired
+	private JwtAuthEntryPoint unauthorizedHandler;
 
-		http
-				.csrf().disable()
-				.authorizeRequests()
-				.antMatchers("/management/api/**").permitAll()
-				.antMatchers("/api/**").hasAnyRole("ADMIN","VIEWER")
-				.anyRequest().authenticated()
-				.and().formLogin().defaultSuccessUrl("/api/v1/trucks/", true)
-				.and().rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(7)).key("asasdflkjsHndgg4132156sdSD")
-				.and().logout().logoutUrl("/logout").clearAuthentication(true).invalidateHttpSession(true);
+	@Bean
+	public JwtAuthenticationFilter jwtAuthenticationFilter() {
+		return new JwtAuthenticationFilter();
+	}
+
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 
 	@Override
@@ -60,5 +77,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		daoAuthenticationProvider.setUserDetailsService(userDetailsService);
 
 		return daoAuthenticationProvider;
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http
+				.cors().disable()
+				.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+				.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.authorizeRequests()
+				.antMatchers("/edge/**").permitAll()
+				.antMatchers("/management/api/**").permitAll()
+				.antMatchers("/api/**").hasAnyRole("ADMIN","VIEWER")
+				.anyRequest().authenticated();
+		http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 }
