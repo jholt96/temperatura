c2afc98de18430fa446262512a776d4a4f40ac57
jholt96 <jholt96@live.com>
2020-07-27 17:43:00 -0400

    added truckservice for the api

--- Temperatura-Frontend/temeratura-frontend/src/Services/trucksService.js
+++ Temperatura-Frontend/temeratura-frontend/src/Services/trucksService.js
@@ -1,12 +1,36 @@
+import axios from 'axios';
+import getAuthHeader from './Services/authHeader';
+import Truck from '../Classes/Truck';
+
 
 class TrucksService {
-    getAllTrucks() {
 
+    getAllTrucks() {
+        var config = { headers: {"Authorization" : getAuthHeader()}};
+        axios.get(API_URL + '/', config ).then((res) =>{
+            var newTrucks = [];
+            
+            res.data.forEach(truck => {
+                newTrucks.push(new Truck(truck));
+                this.truckMap.set(truck.hostname,(newTrucks.length - 1))
+            });
+            this.setState({trucks:newTrucks})
+        },(res) => {
+            console.log("error");
+        });
     }
+
     getOneTruck(hostname) {
+        var config = { headers: {"Authorization" : getAuthHeader()}};
+        axios.get(API_URL + '/' + hostname, config ).then((res) =>{
+            var newTruck = res.data;  
+            this.setState({trucks:newTrucks})
+        },(res) => {
+            console.log("error");
+        });
 
     }
     getAlerts(hostname) {
-        
+
     }
 }
